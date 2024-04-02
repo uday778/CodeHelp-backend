259ebe11b84708de0abb6fc28c243aeef209bbbb
Uday kumar <131101730+uday778@users.noreply.github.com>
2024-04-02 10:37:28 +0530

    fixed typo error

--- week-01/Blog/routes/blog.js
+++ week-01/Blog/routes/blog.js
@@ -17,7 +17,7 @@ router.post('/comments/create',createComment)
 router.post('/posts/create',createPost)
 router.get('/posts',getAllPosts)
 router.post('/likes/like',likePost)
-router.post('/likes//unlike',unlikePost)
+router.post('/likes/unlike',unlikePost)
 
 
 
